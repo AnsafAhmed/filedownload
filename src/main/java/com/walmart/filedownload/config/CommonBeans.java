a72524cafcb3dfb7f0f86a51cbfb7bc43cfd9b49
AnsafAhmed <37632121+AnsafAhmed@users.noreply.github.com>
2022-02-18 11:42:32 +0530

    Added CommonBeans

---
+++ src/main/java/com/walmart/filedownload/config/CommonBeans.java
@@ -0,0 +1,25 @@
+package com.walmart.filedownload.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+@Profile("!test")
+public class CommonBeans {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        //corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsFilter(source);
+    }
+}
